629f52c99932defe0bc36acace5226c12be419b1
Hortênsia Costa de Barcelos <hcb13@users.noreply.github.com>
2023-03-09 09:38:57 -0300

    Updates
    Update UI with the number of fruits collected. Load level is not working

--- CutePlatformerProject/Assets/Scripts/Items/GotCollected.cs
+++ CutePlatformerProject/Assets/Scripts/Items/GotCollected.cs
@@ -2,11 +2,30 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GotCollected : MonoBehaviour
 {
+    private static int amountCollected = 0;
+
+    private void Start()
+    {
+        amountCollected = 0;
+        UpdateTextFruitsCollected();
+    }
+
     public void OnGotCollected()
     {
+        amountCollected++;
+        UpdateTextFruitsCollected();
+
         gameObject.SetActive(false);
+    }
+
+    private void UpdateTextFruitsCollected()
+    {
+        GameObject textFruitsCollected = GameObject.Find("TextFruitsCollected");
+        if (textFruitsCollected != null)
+            textFruitsCollected.GetComponent<TextMeshProUGUI>().text = amountCollected.ToString();
     }
 }
