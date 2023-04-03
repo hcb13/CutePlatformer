045cfa5d94dd5322827507318fe77ea3d455d78d
Hortênsia Costa de Barcelos <hcb13@users.noreply.github.com>
2023-04-03 10:22:50 -0300

    Loading Level Updates

--- CutePlatformerProject/Assets/Scripts/Player/PlayerGoNextLevel.cs
+++ CutePlatformerProject/Assets/Scripts/Player/PlayerGoNextLevel.cs
@@ -10,18 +10,21 @@ public class PlayerGoNextLevel : MonoBehaviour
     [SerializeField]
     private GameObject panelLoadingLevel;
 
+    [SerializeField] private int nextLevel = 0;
+
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("LoadLevel"))
-        {
-            if (panelLoadingLevel != null)
+        {            
+            if (nextLevel != 0)
             {
+                panelLoadingLevel.SetActive(true);
                 StartCoroutine(LoadingLevel());
             }
             else
             {
-                //StartCoroutine(GoToMainMenu());
+                StartCoroutine(GoToMainMenu());
             }
         }
     }
